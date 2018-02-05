a4c460700294f934500ce2e78e01e5faed8039a5
Darya Talanina <dtalanina@gmail.com>
2018-02-05 13:26:56 +0100

    Move Cell component to a separate folder

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
 import BACKGROUNDS from './backgrounds';
+import Cell from './components/Cell';
 import Row from './components/Row';
 
 
@@ -21,54 +22,6 @@ const Legend = () => (
     ))}
   </div>
 );
-
-
-class Cell extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tile: 0,
-      hover: false,
-    };
-  };
-  
-  onChange = (event) => {
-    this.setState({tile: event.target.value})
-  };
-
-  onMouseEnter = () => {
-    this.setState({hover: true})
-  };
-
-  onMouseLeave = () => {
-    this.setState({hover: false})
-  };
-
-  render() {
-    const style = {
-      backgroundImage: BACKGROUNDS[this.state.tile],
-      backgroundSize: 'cover',
-      height: '200px',
-      width: '200px',
-    };
-    return (
-      <div 
-        style={style}
-        onMouseEnter={this.onMouseEnter}
-        onMouseLeave={this.onMouseLeave}
-      >
-        <select 
-          onChange={this.onChange}
-          style={{
-            display: this.state.hover ? 'inline-block' : 'none'
-          }} 
-        >
-          {[...Array(12).keys()].map((e) => <option key={e} value={e}>{e}</option>)}
-        </select>
-      </div>
-    );
-  }
-}
 
 
 class App extends Component {
